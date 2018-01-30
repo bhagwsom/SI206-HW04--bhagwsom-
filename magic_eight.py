ebfa4fc20e23e64e67046a91106ecdc87cba74ed
Somya <bhagwsom@umich.edu>
2018-01-30 18:55:52 -0500

    code to check if user input is a question

--- magic_eight.py
+++ magic_eight.py
@@ -1,1 +1,6 @@
-question = input("What is your question?")
+question = input("What is your question? ")
+while question != "quit":
+
+    if question[-1] != '?':
+        print("I'm sorry, I can only answer questions.")
+    question = input("What is your question? ")
